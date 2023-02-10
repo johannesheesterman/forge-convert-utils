063e69a61e14251137bc7a56d34e20c6a2c6864b
Johannes Heesterman <johannes@elfsquad.io>
2023-02-10 11:52:15 +0100

    release

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export { Reader as SvfReader } from './svf/reader';
+export { Downloader as SvfDownloader } from './svf/downloader';
+export { Downloader as F2dDownloader } from './f2d/downloader';
+export { Writer as GltfWriter } from './gltf/writer';
+export { Region } from 'forge-server-utils/dist/common';
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/svf/downloader.d.ts
@@ -0,0 +1,19 @@
+import { ModelDerivativeClient } from 'forge-server-utils';
+import { IAuthOptions, Region } from 'forge-server-utils/dist/common';
+export interface IDownloadOptions {
+    outputDir?: string;
+    log?: (message: string) => void;
+    failOnMissingAssets?: boolean;
+}
+export interface IDownloadTask {
+    ready: Promise<void>;
+    cancel: () => void;
+}
+export declare class Downloader {
+    protected auth: IAuthOptions;
+    protected modelDerivativeClient: ModelDerivativeClient;
+    constructor(auth: IAuthOptions, host?: string, region?: Region);
+    download(urn: string, options?: IDownloadOptions): IDownloadTask;
+    private _download;
+}
+//# sourceMappingURL=downloader.d.ts.map
